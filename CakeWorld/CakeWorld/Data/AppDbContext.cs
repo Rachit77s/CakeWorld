6b9388f69861d9767d2628a7e6f6ab7e461070c9
Rachit Srivastava <rachitsrivastava77@gmail.com>
2021-04-19 23:54:53 +0530

    Added support for EFCore

---
+++ CakeWorld/CakeWorld/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using CakeWorld.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CakeWorld.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Cake> Cakes { get; set; }
+    }
+}
--- CakeWorld/CakeWorld/Startup.cs
+++ CakeWorld/CakeWorld/Startup.cs
@@ -1,8 +1,11 @@
+using CakeWorld.Data;
 using CakeWorld.Repository.Interfaces;
 using CakeWorld.Repository.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -14,10 +17,23 @@ namespace CakeWorld
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // Rachit: Add SQL Connection
+            services.AddDbContextPool<AppDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
+
             // Rachit: Adds support for MVC in our application
             services.AddControllersWithViews();
 
