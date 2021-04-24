0a5a59e718750bd73b862c61e07a0e584af319cb
Rachit Srivastava <rachitsrivastava77@gmail.com>
2021-04-24 15:51:11 +0530

    Added DB Script

---
+++ CakeWorld/CakeWorld/DB Scripts/DBScript.sql
@@ -0,0 +1,66 @@
+SELECT * FROM CAKES
+
+
+UPDATE CAKES
+SET ImageUrl = 'https://gillcleerenpluralsight.blob.core.windows.net/files/applepie.jpg' , 
+ImageThumbnailUrl = 'https://gillcleerenpluralsight.blob.core.windows.net/files/applepiesmall.jpg'
+Where CakeId = 1
+
+--UPDATE CAKES
+--SET ImageUrl = 'https://gillcleerenpluralsight.blob.core.windows.net/files/blueberrycheesecake.jpg' , 
+--ImageThumbnailUrl = 'https://gillcleerenpluralsight.blob.core.windows.net/files/blueberrycheesecakesmall.jpg'
+--Where CakeId = 2
+
+
+--UPDATE CAKES
+--SET ImageUrl = 'https://gillcleerenpluralsight.blob.core.windows.net/files/cheesecake.jpg' , 
+--ImageThumbnailUrl = 'https://gillcleerenpluralsight.blob.core.windows.net/files/cheesecakesmall.jpg'
+--Where CakeId = 3
+
+UPDATE CAKES
+SET ImageUrl = 'https://gillcleerenpluralsight.blob.core.windows.net/files/cherrypie.jpg' , 
+ImageThumbnailUrl = 'https://gillcleerenpluralsight.blob.core.windows.net/files/cherrypiesmall.jpg'
+Where CakeId = 4
+
+
+
+UPDATE CAKES
+SET ImageUrl = 'https://gillcleerenpluralsight.blob.core.windows.net/files/christmasapplepie.jpg' , 
+ImageThumbnailUrl = 'https://gillcleerenpluralsight.blob.core.windows.net/files/christmasapplepiesmall.jpg'
+Where CakeId = 5
+
+UPDATE CAKES
+SET ImageUrl = 'https://gillcleerenpluralsight.blob.core.windows.net/files/cranberrypie.jpg' , 
+ImageThumbnailUrl = 'https://gillcleerenpluralsight.blob.core.windows.net/files/cranberrypiesmall.jpg'
+Where CakeId = 6
+
+
+
+UPDATE CAKES
+SET ImageUrl = 'https://gillcleerenpluralsight.blob.core.windows.net/files/peachpie.jpg' , 
+ImageThumbnailUrl = 'https://gillcleerenpluralsight.blob.core.windows.net/files/peachpiesmall.jpg'
+Where CakeId = 7
+
+UPDATE CAKES
+SET ImageUrl = 'https://gillcleerenpluralsight.blob.core.windows.net/files/pumpkinpie.jpg' , 
+ImageThumbnailUrl = 'https://gillcleerenpluralsight.blob.core.windows.net/files/pumpkinpiesmall.jpg'
+Where CakeId = 8
+
+
+
+UPDATE CAKES
+SET ImageUrl = 'https://gillcleerenpluralsight.blob.core.windows.net/files/rhubarbpie.jpg' , 
+ImageThumbnailUrl = 'https://gillcleerenpluralsight.blob.core.windows.net/files/rhubarbpiesmall.jpg'
+Where CakeId = 9
+
+UPDATE CAKES
+SET ImageUrl = 'https://gillcleerenpluralsight.blob.core.windows.net/files/strawberrypie.jpg' , 
+ImageThumbnailUrl = 'https://gillcleerenpluralsight.blob.core.windows.net/files/strawberrypiesmall.jpg'
+Where CakeId = 10
+
+
+UPDATE CAKES
+SET ImageUrl = 'https://gillcleerenpluralsight.blob.core.windows.net/files/strawberrycheesecake.jpg' , 
+ImageThumbnailUrl = 'https://gillcleerenpluralsight.blob.core.windows.net/files/strawberrycheesecakesmall.jpg'
+Where CakeId = 11
+
